da35b36dae5ef0cd4ee4247a85d47f60757d98dc
mattl <matt.lee.personal@gmail.com>
2021-02-13 20:25:51 -0600

    add initial dockerfile stuff

--- db/init.sh
+++ db/init.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-psql postgres -c "CREATE DATABASE events OWNER conference_app_user"
-psql postgres -c "CREATE DATABASE accounts OWNER conference_app_user"
-psql postgres -c "CREATE DATABASE badges OWNER conference_app_user"
+# TODO: create separate databases
+psql postgres -c "CREATE DATABASE conference_app OWNER conference_app_user"
+
+# psql postgres -c "CREATE DATABASE events OWNER conference_app_user"
+# psql postgres -c "CREATE DATABASE accounts OWNER conference_app_user"
+# psql postgres -c "CREATE DATABASE badges OWNER conference_app_user"
